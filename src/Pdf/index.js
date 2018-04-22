9ce976c44f2eb026b1e120971f20c80e000c281d
Patrick Dronk <patrickdronk@me.com>
2018-04-22 15:30:57 +0200

    renaming

--- src/Pdf/index.js
+++ src/Pdf/index.js
@@ -86,10 +86,10 @@ class PDF  {
 
   _configurePageSize () {
     if (this._hasPageConfigurationOption('size')) {
-      if (this_.isOfType(this.options.page.size, 'string')) {
+      if (this._isOfType(this.options.page.size, 'string')) {
         this.definition.pageSize = this.options.page.size
       }
-      if (this_.isOfType(this.options.page.size, 'object')) {
+      if (this._isOfType(this.options.page.size, 'object')) {
         if (
           this.options.page.size.hasOwnProperty('width') &&
           this.options.page.size.hasOwnProperty('height')
